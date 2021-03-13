2fd81c51969d36aba9068ff14854b0421cecffd1
Brian Connolly <Brian.connolly71@gmail.com>
2021-03-13 09:08:27 +0000

    added pages

--- run.py
+++ run.py
@@ -7,10 +7,25 @@ app = Flask(__name__)#instance of flask with the name of the applications module
 def index():
     return render_template("index.html")
 
+@app.route("/about")#top level domain
+def about():
+    return render_template("about.html")
+
+
+#2 lines for pep8 compliance
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":#main is the default module in python
     app.run(
-        use_debugger=False, 
+        use_debugger=True, 
         use_reloader=False, 
         passthrough_errors=True
     )
