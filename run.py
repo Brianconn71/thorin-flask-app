73cde22c07fb403e771c63bff41d9e0d59e16eeb
Brian Connolly <Brian.connolly71@gmail.com>
2021-03-14 18:04:23 +0000

    setup of foor loop and if statement in jinja templating

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)#instance of flask with the name of the applications module, flask needs this so it knows where to look in templates and static files
@@ -9,18 +10,21 @@ def index():
 
 @app.route("/about")#top level domain
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 #2 lines for pep8 compliance
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":#main is the default module in python
